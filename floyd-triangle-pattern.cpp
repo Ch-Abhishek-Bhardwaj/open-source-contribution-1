d45dfd20a04ddfba95380d4b4f7d5c51f89c0bb0
Abhishek <abhirai2006a@gmail.com>
2025-06-15 20:22:11 +0530

    Add floyd-triangle-pattern

---
+++ floyd-triangle-pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num=1;
+    int n;
+    cout<<"Enter the value of num:-";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;
+        j++){
+           cout<<num;
+
+            num++;
+        }
+        cout<<endl;
+    }
+}
